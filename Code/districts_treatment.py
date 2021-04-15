f7d62d0dca3dfbec6052927b6c8372dbceb15092
m2016713 <r2016713@novaims.unl.pt>
2021-04-15 16:11:15 +0100

    Update Code Districts Treatment

--- Code/districts_treatment.py
+++ Code/districts_treatment.py
@@ -10,64 +10,61 @@ Data Treatment - Domestic Violence Districts Portugal
 
 import pandas as pd
 import numpy as np
+import os, inspect
+os.chdir(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 
-#importar dados
-data = pd.read_csv(r'C:\Users\anacs\Documents\NOVA IMS\Mestrado\Tese\Dados\Distrito.csv', delimiter=';')
+#Importing Data
+data = pd.read_csv(r'Data\Districts.csv', delimiter=';')
 
-#eliminar linhas irrelevantes
+#Eliminate Irrelevant Rows
 data = data.iloc[5:]
 data.reset_index(drop=True, inplace=True)
 
-#remover colunas só com null e colunas desnecessárias
+#Removing Null and Unnecessary Columns
 data.dropna(axis=1, how='all', inplace=True)
 data.drop([data.columns[0]], axis=1, inplace=True)
 
-#colocar titulos nas colunas
+#Creating Titles for Columns
 data.columns = data.iloc[0]
 data = data.iloc[1:]
-data.rename(columns={data.columns[0]: "Tipo_Crime"}, inplace=True)
+data.rename(columns={data.columns[0]: "Crime", "District": "District"}, inplace=True)
 
-#substituir nulls da coluna Tipo_Crime
-data['Tipo_Crime'] = data['Tipo_Crime'].fillna(method='ffill')
+#Replacing Nulls in Crime Column
+data['Crime'] = data['Crime'].fillna(method='ffill')
 
-#remover caracteres desconhecidos da coluna Tipo_Crime e da coluna Distrito
-data['Tipo_Crime'] = data['Tipo_Crime'].str.replace("[^A-Za-z1234567890'-. ]", "")
-data['Distrito'] = data['Distrito'].str.replace("[^A-Za-z1234567890'-. ]", "")
-data['Distrito'] = data['Distrito'].str.replace("vora", "Evora")
-data['Distrito'] = data['Distrito'].str.replace("Bragana", "Braganca")
-data['Distrito'] = data['Distrito'].str.replace("Santarm", "Santarem")
-data['Distrito'] = data['Distrito'].str.replace("Setbal", "Setubal")
-data['Distrito'] = data['Distrito'].str.replace("So Miguel", "Sao Miguel")
-data['Distrito'] = data['Distrito'].str.replace("So Jorge", "Sao Jorge")
+#Removing Unknown Characters from District and Crime
+data['Crime'] = data['Crime'].str.replace("[^A-Za-z1234567890'-. ]", "")
+data['District'] = data['District'].str.replace("[^A-Za-z1234567890'-. ]", "")
+data['District'] = data['District'].str.replace("vora", "Evora")
+data['District'] = data['District'].str.replace("Bragana", "Braganca")
+data['District'] = data['District'].str.replace("Santarm", "Santarem")
+data['District'] = data['District'].str.replace("Setbal", "Setubal")
+data['District'] = data['District'].str.replace("So Miguel", "Sao Miguel")
+data['District'] = data['District'].str.replace("So Jorge", "Sao Jorge")
 
-#manter apenas violencia domestica
-data = data[data['Tipo_Crime'].str.contains('violncia domstica') | data['Tipo_Crime'].str.contains('Violncia domstica')]
+#Keeping Only Domestic Violence
+data = data[data['Crime'].str.contains('violncia domstica') | data['Crime'].str.contains('Violncia domstica')]
 
-#corrigir nulls
+#Correct Nulls
 data = data.replace('..', np.nan)
 data.dropna(axis=1, how='all', inplace=True)
-data ['Distrito'] = data['Distrito'].replace(np.nan, 'Nacional')
+data ['District'] = data['District'].replace(np.nan, 'Nacional')
 
-#corrigir coluna Tipo_Crime
-data['Tipo_Crime'] = data['Tipo_Crime'].replace('Violncia domstica cnjugeanlogo', 'Violencia Domestica Conjuge ou Analogo')
-data['Tipo_Crime'] = data['Tipo_Crime'].replace('Violncia domstica contra menores', 'Violencia Domestica Menores')
-data['Tipo_Crime'] = data['Tipo_Crime'].replace('Outros violncia domstica', 'Outros Violencia Domestica')
+#Correct Crime Column
+data['Crime'] = data['Crime'].replace('Violncia domstica cnjugeanlogo', 'Violencia Domestica Conjuge ou Analogo')
+data['Crime'] = data['Crime'].replace('Violncia domstica contra menores', 'Violencia Domestica Menores')
+data['Crime'] = data['Crime'].replace('Outros violncia domstica', 'Outros Violencia Domestica')
 
-#meter os anos por ordem crescente
+#Sort Years in Ascending Order
 data = data[data.columns[::-1]]
 cols = data.columns.tolist()
 cols = cols[-2:] + cols[:-2]
 data = data[cols]
 del cols
 
-#remover linhas de total
-data = data[~(data['Tipo_Crime'].str.contains('Total'))]
+#Removing Total Rows
+data = data[~(data['Crime'].str.contains('Total'))]
 
-#Exportar para CSV :)
-data.to_csv(r'C:\Users\anacs\Documents\NOVA IMS\Mestrado\Tese\Dados\distritos_tratados.csv', index=False)
+#Exportar as CSV
+data.to_csv(r'Data\districts_treated.csv', index=False)
 
-#TESTAR CSV
-data = pd.read_csv(r'C:\Users\anacs\Documents\NOVA IMS\Mestrado\Tese\Dados\distritos_tratados.csv')
-
-# GOOD TO GO!#
-#SUBSTITUIR POR 0 OS VALORES NULL?
