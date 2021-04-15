68a9527c1c24932844e785ac13213fc5ae6916da
m2016713 <r2016713@novaims.unl.pt>
2021-04-15 15:23:55 +0100

    Creating DVASA Dataset

--- Code/DVASA_dataset.py
+++ Code/DVASA_dataset.py
@@ -13,30 +13,31 @@ import os, inspect
 os.chdir(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 
 #Importing Domestic Violence Data
-data = pd.read_csv(r'Data\municipios_tratados.csv')
+data = pd.read_csv(r'Data\municipalities_treated.csv')
 
 #Eliminate Rows Where the Municipality is not Defined (N.E.)
-ne = data.loc[data.Municipio == "N.E."]
-ne = ne.loc[ne.Tipo_Crime =='Violencia Domestica Conjuge ou Analogo'].values.flatten().tolist()[2:]
-data = data[data['Municipio']!='N.E.']
+ne = data.loc[data.Municipality == "N.E."]
+ne = ne.loc[ne.Crime =='Violencia Domestica Conjuge ou Analogo'].values.flatten().tolist()[2:]
+data = data[data['Municipality']!='N.E.']
 
 #Look for Municipalities that do not Have all Categories
-municipios = data.Municipio.value_counts()
+municipios = data.Municipality.value_counts()
+del municipios
 
 #Counting Null Values by Category
-for cat in list(data.Tipo_Crime.unique()):
-    nan = data[data['Tipo_Crime']==cat].isna().sum().sum()
+for cat in list(data.Crime.unique()):
+    nan = data[data['Crime']==cat].isna().sum().sum()
     print(str(cat) + ': ' + str(nan))
 del cat
 
 #Removing all Categories Except for DVASA
-data = data[data['Tipo_Crime']=='Violencia Domestica Conjuge ou Analogo']
-data.drop(columns=['Tipo_Crime'], inplace=True)
+data = data[data['Crime']=='Violencia Domestica Conjuge ou Analogo']
+data.drop(columns=['Crime'], inplace=True)
 
 #See Difference Between National Total and Municipalities Total for DVASA Category
-data_total = pd.read_csv(r'C:\Users\anacs\Documents\NOVA IMS\Mestrado\Tese\Dados\nacional_tratados.csv')
+data_total = pd.read_csv(r'Data\national_treated.csv')
 
-total = data_total.loc[data_total.Tipo_Crime == "Violencia Domestica Conjuge ou Analogo"].values.flatten().tolist()[1:]
+total = data_total.loc[data_total.Crime == "Violencia Domestica Conjuge ou Analogo"].values.flatten().tolist()[1:]
 total_mun = data.sum(numeric_only=True, axis=0).tolist()
 diference = [a-b for a,b in zip(total, total_mun)]
 diference = [a-b for a,b in zip(diference, ne)]
@@ -46,10 +47,10 @@ del total, total_mun
 data.fillna(1, inplace=True)
 
 #Turn Years into One Column
-data = data.melt(id_vars=["Municipio"], var_name="Ano", value_name="Value")
-data['Ano'] = data['Ano'].apply(pd.to_numeric)
+data = data.melt(id_vars=["Municipality"], var_name="Year", value_name="Value")
+data['Year'] = data['Year'].apply(pd.to_numeric)
 
-del data_total, diference, municipios, ne
+del data_total, diference, ne
 
 #Export Final DVASA Dataset
 data.to_csv(r'Data\DVASA.csv', index=False)
